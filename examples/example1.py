d6881855341fa5b9b8dee780386caa8d0172a054
aprimera <alejandro.primera@gmail.com>
2020-05-16 07:04:09 -0500

    Example 1 now fully working and exporting plots includign drive indices Issues were mainly related to input data errors.

--- examples/example1.py
+++ examples/example1.py
@@ -7,7 +7,9 @@ import pandas as pd
 import os
 import datetime
 import libraries.plots as plots
+from plotly.subplots import make_subplots
 from plotly.offline import plot
+import webbrowser
 
 
 regress = False
@@ -25,8 +27,6 @@ df_prod['datestamp'] = [datetime.timedelta(day)+startdate for day in df_prod['Da
 df_prod['wi'] = 0
 df_prod['gi'] = 0
 df_prod['gp'] = df_prod['gp']*1000.0
-
-
 keep_cols = ['Pressure\npsia', 'Bo\nrb/stbo', 'Rs\nscf/stbo', 'Bg rb/mcf', 'Bt rb/stbo']
 df_pvt_o = pd.read_excel(file, sheet_name='Oil PVT', skiprows=3, nrows=16)
 df_pvt_o.rename(columns={'Pressure\npsia': 'pressure', 'Bo\nrb/stbo': 'oil_fvf', 'Rs\nscf/stbo': 'solution_gas'}, inplace=True)
@@ -34,14 +34,14 @@ keep_cols = ['Pressure psia', 'z', 'Bg\nrb/mcf']
 df_pvt_g = pd.read_excel(file, sheet_name='Z Factors', skiprows=3, nrows=12)
 df_pvt_g.rename(columns={'Pressure\npsia': 'pressure', 'Bg\nrb/mcf': 'gas_fvf'}, inplace=True)
 tank = {
-    'initial_inplace': 13E6,
+    'initial_inplace': 13.8E6,
     'initial_gascap': 0,
     'initial_pressure': 10180,
-    'wei': 140e6,
+    'wei': 141.8e6,
     'J': 0.93,
     'swi': 0.2,
     'cw': 2.5e-6,
-    'cf': 3e-6,
+    'cf': 3e-5,
     'Boi': 1.735,
     'Bgi': 0.6508
 }
@@ -51,31 +51,21 @@ pvt_master = {
     'temperature': 219,
 }
 
-
-
-#df_prod = pd.DataFrame(read_frame(instance_prod))
-#df_pvt_o = pd.DataFrame(read_frame(instance_pvt_o))
-#df_pvt_g = pd.DataFrame(read_frame(instance_pvt_g))
-# df_tank_master = pd.DataFrame(read_frame(instance_tank))
-# df_pvt_master = pd.DataFrame(read_frame(instance_pvt_master))
-#regress_config = {}
-#list_regress = ['initial_inplace_regress', 'aquifer_size_regress', 'aquifer_pi_regress']
-#for var in list_regress:
-#    try:
-#        regress_config[var] = instance_tank[var][0]
-#        regress = True
-#    except:
-#        pass
-
-#regress = True
-ts, Pres_calc, ts_obs, reservoir_pressure_obs, DDI, SDI, WDI, CDI, N, Wei, J = main.matbal_run2(tank, df_prod,
+ts, Pres_calc, ts_obs, reservoir_pressure_obs, DDI, SDI, WDI, CDI, N, Wei, J = main.matbal_run(tank, df_prod,
                         pvt_master, df_pvt_o, df_pvt_g, regress, regress_config)
 
-plot1 = plots.plot_pressure_match(ts, Pres_calc, ts_obs, reservoir_pressure_obs)
-plot(plot1)
 
+# Dashboard
+html_graphs=open("dashboard_ex1.html", 'w')
+html_graphs.write("<html><head></head><body>"+"\n")
+plot1 = plots.plot_pressure_match(ts, Pres_calc, ts_obs, reservoir_pressure_obs)
+plot(plot1, filename='plot1.html', auto_open=False)
+html_graphs.write("  <object data=\""+'plot1.html'+"\" width=\"650\" height=\"500\"></object>"+"\n")
 plot2 = plots.plot_drive_indices(ts, DDI, SDI, WDI, CDI)
-plot(plot2)
+plot(plot2, filename='plot2.html', auto_open=False)
+html_graphs.write("  <object data=\""+'plot2.html'+"\" width=\"650\" height=\"500\"></object>"+"\n")
+html_graphs.write("</body></html>")
+print('Open exported html dashboard')
 
 
 
