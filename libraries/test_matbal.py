56dfa5d2e515e23d29683c84b1bb22dc4814cd96
Lodewikes <greyvenstein.louw@gmail.com>
2020-05-19 17:02:29 +0200

    wrote test for zeroDivion/Np=0

---
+++ libraries/test_matbal.py
@@ -0,0 +1,82 @@
+import sys
+sys.path.insert(1, './')
+import libraries.matbal as mb
+from libraries.matbal import raisedError
+import pytest
+import numpy as np
+import pandas as pd
+
+# Formation_total_volume factor START:
+def test_formation_total_volume_factor_higher():
+    Bo, Bg, Rsb, Rs = 1.735, 0.6508, 1720, 1720
+    fvft = mb.formation_total_volume_factor(Bo, Bg, Rsb, Rs)
+    assert fvft == Bo
+
+def test_formation_total_volume_factor_lower():
+    Bo, Bg, Rsb, Rs = 1.735, 0.6508, 1720, 1720
+    test_val = Bo + Bg * (Rsb - Rs)
+    fvft = mb.formation_total_volume_factor(Bo, Bg, Rsb, Rs)
+    assert fvft == test_val
+# Formation_total_volume factor END:
+
+
+#--------------Formation_volume_factor_gas_from_z START:
+
+#------TEST DIV by ZERO------#
+def test_production_injection_balance_zero_div():
+    """ Test to see if Np is zero""" 
+    
+    # Arrange
+    Np = 0
+    Bt = 1.735
+    Rs = 1720
+    Rsi = 1720
+    Bg = 0.00064
+    Wp = 0
+    Bw = 1.0
+    Winj = 0
+    Bwinj = 1 
+    Ginj = 0
+    Bginj = 0.00064
+    Gp = 3000000
+
+    # Act & Assert
+    with pytest.raises(raisedError) as context:
+        f, produced_oil_and_gas, produced_water, injected_gas, injected_water = mb.production_injection_balance(Np,
+                                                                                 Bt, Rs, Rsi, Bg, Wp, Bw, 
+                                                                                 Winj, Bwinj, Ginj, Bginj, Gp)
+
+#------RESOLVED------#
+
+#def test_production_injection_balance():
+#    
+#    # Arrange
+#    Np = 4000
+#    Bt = 1.735
+#    Rs = 1720
+#    Rsi = 1720
+#    Bg = 0.00064
+#    Wp = 0
+#    Bw = 1.0
+#    Winj = 0
+#    Bwinj = 1 
+#    Ginj = 0
+#    Bginj = 0.00064
+#    Gp = 3000000
+#
+#    # Act
+#    f, produced_oil_and_gas, produced_water, injected_gas, injected_water = mb.production_injection_balance(Np, 
+#                                                                                 Bt, Rs, Rsi, Bg, Wp, Bw, 
+#                                                                                 Winj, Bwinj, Ginj, Bginj, Gp)
+#
+#
+#    assert produced_oil_and_gas >= 0
+#    assert produced_water >= 0
+#    assert injected_water >= 0
+#    assert injected_gas >= 0 
+#    assert produced_oil_and_gas == 4456.8
+
+#--------------Formation_volume_factor_gas_from_z END:
+
+
+
