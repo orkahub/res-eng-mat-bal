17a08460725b283f18273716e1cf15e0452ff57b
Lodewikes <greyvenstein.louw@gmail.com>
2020-05-20 11:14:32 +0200

    def test_production_injection_balance_negatives: test for negative reservoir volume inputs

--- libraries/test_matbal.py
+++ libraries/test_matbal.py
@@ -19,8 +19,9 @@ def test_formation_total_volume_factor_lower():
     assert fvft == test_val
 # Formation_total_volume factor END:
 
-
-#--------------Formation_volume_factor_gas_from_z START:
+#------------------------------------------------------
+#--------------production injection balance START:
+#------------------------------------------------------
 
 #------TEST DIV by ZERO------#
 def test_production_injection_balance_zero_div():
@@ -48,35 +49,49 @@ def test_production_injection_balance_zero_div():
 
 #------RESOLVED------#
 
-#def test_production_injection_balance():
-#    
-#    # Arrange
-#    Np = 4000
-#    Bt = 1.735
-#    Rs = 1720
-#    Rsi = 1720
-#    Bg = 0.00064
-#    Wp = 0
-#    Bw = 1.0
-#    Winj = 0
-#    Bwinj = 1 
-#    Ginj = 0
-#    Bginj = 0.00064
-#    Gp = 3000000
-#
-#    # Act
-#    f, produced_oil_and_gas, produced_water, injected_gas, injected_water = mb.production_injection_balance(Np, 
-#                                                                                 Bt, Rs, Rsi, Bg, Wp, Bw, 
-#                                                                                 Winj, Bwinj, Ginj, Bginj, Gp)
-#
-#
+#------TEST NEGATIVE VALUES------#
+def test_production_injection_balance_negatives():
+   """Test for negative reservoir volume inputs""" 
+
+    # Arrange
+    Np = 4000
+    Bt = 1.735
+    Rs = 1720
+    Rsi = 1720
+    Bg = 0.00064
+    Wp = 0
+    Bw = 1.0
+    Winj = 0
+    Bwinj = 1 
+    Ginj = 0
+    Bginj = 0.00064
+    Gp = -3000000
+
+    # Act
+
+    with pytest.raises(raisedError) as context:
+        f, produced_oil_and_gas, produced_water, injected_gas, injected_water = mb.production_injection_balance(Np, 
+                                                                                 Bt, Rs, Rsi, Bg, Wp, Bw, 
+                                                                                 Winj, Bwinj, Ginj, Bginj, Gp)
+
+#------RESOLVED------#
+
 #    assert produced_oil_and_gas >= 0
 #    assert produced_water >= 0
 #    assert injected_water >= 0
 #    assert injected_gas >= 0 
 #    assert produced_oil_and_gas == 4456.8
 
-#--------------Formation_volume_factor_gas_from_z END:
+#------------------------------------------------------
+#--------------Production injection balance END:
+#------------------------------------------------------
 
 
+#------------------------------------------------------
+#--------------New Func START:
+#------------------------------------------------------
 
+
+#------------------------------------------------------
+#--------------New Func END:
+#------------------------------------------------------
